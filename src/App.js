9e9e012ad8fecbe2c854ffaee7e017b2e434212a
mprince23 <princemalaviya5@gmail.com>
2024-12-26 16:12:49 +0530

    create about2 and about5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import About from "./component/about";
+import About from "./pages/about";
 
 const App = () => {
-  return <div>
-    <About />
-  </div>;
+  return (
+    <div>
+      <About />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/about.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import About2 from "../component/about/about2";
+import About5 from "../component/about/about5";
+
+const About = () => {
+  return (
+    <div>
+      <About2 />
+      <About5 />
+    </div>
+  );
+};
+
+export default About;
